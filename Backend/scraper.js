19d99c4d318a82b46a2eba66aeb166bf6d377a9f
Kalpit Thakur <132124428+veer-kalpit@users.noreply.github.com>
2024-10-01 00:49:55 +0530

    updates

--- Backend/scraper.js
+++ Backend/scraper.js
@@ -4,7 +4,7 @@ const cheerio = require("cheerio");
 const getNews = async () => {
   try {
     // Fetch the HTML from the Navbharat Times website
-    const { data } = await axios.get("https://navbharattimes.indiatimes.com/");
+    const { data } = await axios.get("https://timesofindia.indiatimes.com/");
     const $ = cheerio.load(data);
     let articles = [];
 
@@ -16,7 +16,7 @@ const getNews = async () => {
       // Construct the full link if it's a relative URL
       const fullLink = link.startsWith("http")
         ? link
-        : `https://navbharattimes.indiatimes.com${link}`;
+        : `https://timesofindia.indiatimes.com/${link}`;
 
       // Only add valid entries (non-empty title)
       if (title) {

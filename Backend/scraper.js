55dc495bc18be7514a68719d5d83ae04e7a0c87e
Kalpit Thakur <veerkalpit@gmail.com>
2024-12-06 10:40:14 +0530

    news

--- Backend/scraper.js
+++ Backend/scraper.js
@@ -4,7 +4,7 @@ const cheerio = require("cheerio");
 const getNews = async () => {
   try {
     // Fetch the HTML from the Times of India website
-    const { data } = await axios.get("https://timesofindia.indiatimes.com/");
+    const { data } = await axios.get("https://www.thehindu.com/");
     const $ = cheerio.load(data);
     let articles = [];
 
@@ -16,7 +16,7 @@ const getNews = async () => {
       // Construct the full link if it's a relative URL
       const fullLink = link.startsWith("http")
         ? link
-        : `https://timesofindia.indiatimes.com/${link}`;
+        : `https://www.thehindu.com/${link}`;
 
       // Only add valid entries (non-empty title)
       if (title) {
